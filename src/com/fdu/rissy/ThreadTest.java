67d68e1fc655b8b588061c87e831d175484a9f86
Rissy Lin <lsj.ruihan@gmail.com>
2017-04-19 00:56:47 +0800

    i++ counter operation
    i++ is not atomic. Result may be less than 10000, since some i++ may be override.
    volatile can not guarantee atomic, only visibility
    1)Use synchronized/lock - Reentrant WriteLock
    2)Use AtomicInteger

--- src/com/fdu/rissy/ThreadTest.java
+++ src/com/fdu/rissy/ThreadTest.java
@@ -1,7 +1,16 @@
 package com.fdu.rissy;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by lruihan on 4/18/17.
+ */
+/**
+ * Using Thread.yield() couldn't guarantee executing sequentially
+ * Guarantee sequential
+ * 1)use synchronized on the Class Object, not instance Object
+ * 2)use lock object
  */
 public class ThreadTest {
     private static final int startIndex = 0;
@@ -13,5 +22,33 @@ public class ThreadTest {
         t1.start();
         t2.start();
         t3.start();
+    }
+}
+class MyThread extends Thread {
+
+    private int startIndex;
+    private int nThread;
+    private int endIndex;
+
+    private static final Lock lock = new ReentrantLock();
+
+    public MyThread(int startIndex, int nThread, int endIndex) {
+        this.startIndex = startIndex;
+        this.nThread = nThread;
+        this.endIndex = endIndex;
+    }
+
+    @Override
+    public void run() {
+        // synchronized (MyThread.class) {
+        lock.lock();
+        try {
+            for (int i = startIndex; i < endIndex; i += nThread) {
+                System.out.println("[Thread " + this.getId() + "]" + i);
+                //Thread.yield();
+            }
+        }finally {
+            lock.unlock();
+        }
     }
 }
