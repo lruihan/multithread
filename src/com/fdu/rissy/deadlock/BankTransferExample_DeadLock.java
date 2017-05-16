cb964fd475167aaa2a994d6b791e2f362e560522
Rissy Lin <lsj.ruihan@gmail.com>
2017-05-17 00:35:13 +0800

    BankTransfer example to hanlde deadlock
    1) Use one shared lock, when all finished for one thread, then release lock
    
    2) Use same lock order
    
    3) Use ReentrantLock.tryLock(), if failed, it will release the first lock already held

---
+++ src/com/fdu/rissy/deadlock/BankTransferExample_DeadLock.java
@@ -0,0 +1,41 @@
+package com.fdu.rissy.deadlock;
+
+public class BankTransferExample_DeadLock {
+
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(50000);
+        BankAccount account2 = new BankAccount(60000);
+        BankAccount.initiateTransfer(account1, account2, 1000);
+        BankAccount.initiateTransfer(account2, account1, 1000);
+    }
+    static class BankAccount {
+        private double balanceAmount;
+
+        public BankAccount(double balance) {
+            this.balanceAmount = balance;
+        }
+
+        private void depositAmount(BankAccount to, double amount) {
+            //deadlock, when 'to' transfer to 'this'
+            synchronized (this) {
+                synchronized (to) {
+                    if (amount > balanceAmount) {
+                        throw new IllegalArgumentException("Transfer can't be completed");
+                    }
+                    to.balanceAmount += amount;
+                    this.balanceAmount -= amount;
+                }
+            }
+        }
+
+        public static void initiateTransfer(final BankAccount first, final BankAccount second, final double amount) {
+
+            Thread transfer = new Thread(() -> {
+                first.depositAmount(second, amount);
+            });
+            transfer.start();
+        }
+    }
+
+}
+
---
+++ src/com/fdu/rissy/deadlock/BankTransferExample_Fix_OneLock.java
@@ -0,0 +1,38 @@
+package com.fdu.rissy.deadlock;
+
+public class BankTransferExample_Fix_OneLock {
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(50000);
+        BankAccount account2 = new BankAccount(60000);
+        BankAccount.initiateTransfer(account1, account2, 1000);
+        BankAccount.initiateTransfer(account2, account1, 1000);
+    }
+
+    static final class BankAccount {
+        private double balanceAmount;
+        //share one lock
+        private static final Object lock = new Object();
+
+        public BankAccount(double balance) {
+            this.balanceAmount = balance;
+        }
+
+        private void depositAmount(BankAccount to, double amount) {
+            synchronized (lock) {
+                if (amount > balanceAmount) {
+                    throw new IllegalArgumentException("Transfer can't be completed");
+                }
+                to.balanceAmount += amount;
+                this.balanceAmount -= amount;
+            }
+        }
+
+        public static void initiateTransfer(final BankAccount first, final BankAccount second, final double amount) {
+
+            Thread transfer = new Thread(() -> {
+                first.depositAmount(second, amount);
+            });
+            transfer.start();
+        }
+    }
+}
